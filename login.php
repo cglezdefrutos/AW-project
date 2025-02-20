383ccdc50e18d771fcd2c492444d328543f660a7
cglezdefrutos <cglezdefrutos@gmail.com>
2025-02-21 00:51:50 +0100

    se añade la estructura de formularios

--- login.php
+++ login.php
@@ -1,3 +1,18 @@
 <?php
-// TODO
+    session_start();
+
+    include("includes/login/loginForm.php");
+
+    $titlePage = 'Acceso al sistema';
+
+    $form = new loginForm(); 
+
+    $htmlFormLogin = $form->Manage();
+
+    $mainContent = <<<EOS
+        <h1>Inicio de Sesión</h1>
+        $htmlFormLogin
+    EOS;
+
+    require("includes/views/template/template.php");
 ?>
--- register.php
+++ register.php
@@ -1,3 +1,18 @@
 <?php
-// TODO
+    session_start();
+
+    include("includes/login/registerForm.php");
+
+    $titlePage = 'Registro en el sistema';
+
+    $form = new registerForm(); 
+
+    $htmlFormRegistro = $form->Manage();
+
+    $mainContent = <<<EOS
+        <h1>Registro de usuario</h1>
+        $htmlFormRegistro
+    EOS;
+
+    require("includes/views/template/template.php");
 ?>
