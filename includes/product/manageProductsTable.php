2a196a37163bc1ff8c98ed9318ccbc86744316f9
miguelsnchz <migsan15@ucm.es>
2025-03-26 18:46:24 +0100

    Clases creadas y clase productDTO y managePrductsTable implementadas

---
+++ includes/product/manageProductsTable.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace TheBalance\event;
+
+use TheBalance\views\common\baseTable;
+
+class manageProductsTable extends baseTable
+{
+    protected function generateTableContent(): string
+    {
+        $html = '';
+
+        // Generar filas de la tabla con productos activos
+        foreach ($this->data as $product) {
+            $html .= '<tr>';
+            $html .= '<td>' . htmlspecialchars($product->getProviderId()) . '</td>';
+            $html .= '<td>' . htmlspecialchars($product->getName()) . '</td>';
+            $html .= '<td>' . htmlspecialchars($product->getDescription()) . '</td>';
+            $html .= '<td>' . htmlspecialchars($product->getPrice()) . '</td>';
+            $html .= '<td>' . htmlspecialchars($product->getStock()) . '</td>';
+            $html .= '<td>' . htmlspecialchars($product->getCategoryId()) . '</td>';
+            $html .= '<td>' . htmlspecialchars($product->getImageUrl()) . '</td>';
+            $html .= '<td>' . htmlspecialchars($product->getCreatedAt()) . '</td>';
+            $html .= '<td>';
+            $html .= '<a href="updateProducts.php?productId=' . htmlspecialchars($product->getId()) . '">Editar</a>';
+            $html .= ' o ';
+            $html .= '<a href="manageProducts.php?productId=' . htmlspecialchars($product->getId()) . '" onclick="return confirm(\'¿Estás seguro de que deseas eliminar este producto?\');">Eliminar</a>';
+            $html .= '</td>';
+            $html .= '</tr>';
+        }
+
+        return $html;
+    }
+
+}
---
+++ includes/product/productDTO.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace TheBalance\product;
+
+/**
+ * Data Transfer Object de producto
+ */
+class productDTO implements \JsonSerializable
+{
+    /**
+     * @var int Identificador del producto
+     */
+    private $id;
+
+    /**
+     * @var int Identificador del proveedor propietario del producto
+     */
+    private $provider_id;
+
+    /**
+     * @var string Nombre del producto
+     */
+    private $name;    
+
+    /**
+     * @var string Descripción del producto
+     */
+    private $description;
+
+    /**
+     * @var float Precio del producto
+     */
+    private $price;
+
+    /**
+     * @var int Stock del producto
+     */
+    private $stock;
+
+    /**
+     * @var int Identificador de la categoría del producto
+     */
+    private $category_id;
+
+    /**
+     * @var string URL de la imagen del producto
+     */
+    private $image_url;
+
+    /**
+     * @var string Fecha de creación del producto
+     */
+    private $created_at;
+
+    /**
+     * Constructor
+     */
+    public function __construct($id, $provider_id, $name, $description, $price, $stock, $category_id, $image_url, $created_at)
+    {
+        $this->id = $id;
+        $this->provider_id = $provider_id;
+        $this->name = $name;
+        $this->description = $description;
+        $this->price = $price;
+        $this->stock = $stock;
+        $this->category_id = $category_id;
+        $this->image_url = $image_url;
+        $this->created_at = $created_at;
+    }
+
+    /**
+     * Getters
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getProviderId()
+    {
+        return $this->provider_id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function getStock()
+    {
+        return $this->stock;
+    }
+
+    public function getCategoryId()
+    {
+        return $this->category_id;
+    }
+
+    public function getImageUrl()
+    {
+        return $this->image_url;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Setters
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function setProviderId($provider_id)
+    {
+        $this->provider_id = $provider_id;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    public function setStock($stock)
+    {
+        $this->stock = $stock;
+    }
+
+    public function setCategoryId($category_id)
+    {
+        $this->category_id = $category_id;
+    }
+
+    public function setImageUrl($image_url)
+    {
+        $this->image_url = $image_url;
+    }
+
+    public function setCreatedAt($created_at)
+    {
+        $this->created_at = $created_at;
+    }
+
+    /**
+     * Implementación de JsonSerializable
+     * @return array Array con los datos del objeto
+     */
+    public function jsonSerialize()
+    {
+        return get_object_vars($this);
+    }
+}
