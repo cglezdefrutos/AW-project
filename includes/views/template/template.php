0d9c2a3086f7ec6cadaeb1bf3e348cd16a4c674b
cglezdefrutos <cglezdefrutos@gmail.com>
2025-03-27 02:02:23 +0100

    Se agregan más cambios en los estilos.

--- includes/views/template/template.php
+++ includes/views/template/template.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">    <link rel="stylesheet" type="text/css" href="CSS/styles.css" />
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="/AW-project/CSS/styles.css">
         <link rel="icon" type="image/png" href="img/logo_thebalance.png">
         <title><?=$titlePage?></title>
     </head>
@@ -23,11 +24,9 @@
             </div>
 
             <div class="row align-items-center mb-4">
-                <div class="col-12"> 
-                    <main>
-                        <?= $mainContent ?>
-                    </main>
-                </div>
+                <main class="col">
+                    <?= $mainContent ?>
+                </main>
             </div>
 
             <footer class="row bg-dark text-white p-0">
