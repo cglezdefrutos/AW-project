f5d68f2e64e5bb16ca5f5a6cc5c22d5dffd3c965
cglezdefrutos <137298402+cglezdefrutos@users.noreply.github.com>
2025-04-07 03:32:34 +0200

    Update template.php

--- includes/views/template/template.php
+++ includes/views/template/template.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="CSS/styles.css" />
+    <link rel="stylesheet" type="text/css" href="css/styles.css" />
     <link rel="icon" type="image/png" href="img/logo_thebalance.png">
     <title><?=$titlePage?></title>
 </head>
